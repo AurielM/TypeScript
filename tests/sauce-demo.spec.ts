6b72dae29f88282b6b2b943ec75be7eba62e92e6
AurielM <a.mcshaw@live.co.uk>
2025-02-16 10:48:13 +0000

    third  test under Inventory item page behaviour added and second and third test under Inventory item page behaviour test approach comments added

--- tests/sauce-demo.spec.ts
+++ tests/sauce-demo.spec.ts
@@ -302,5 +302,25 @@ test.describe('Inventory item page behaviour', () => {
     }
 
   });
+  test('Filter button behaviour', async ({ page }) => {
+    const inventoryItemPage = new InventoryItemPage(page);
 
+    // click filter button - expected filtersare present - filter opens/closes
+    
+    //click name button - filter opens/closes
+
+    // click dropdown button - filter opens/closes
+
+    // test results of filter in dropdown - A to Z
+
+    // test results of filter in dropdown - Z to A
+
+    // test results of filter in dropdown - low to high
+    
+    // test results of filter in dropdown - high to low
+
+
+
+
+});
 });
